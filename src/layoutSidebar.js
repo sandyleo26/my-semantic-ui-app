59848c292ab9c4864f0c00255055b20188b7535f
Sha Liu <sha@snug.com>
2018-10-12 22:53:49 +1100

    fine tune sidebar

--- src/layoutSidebar.js
+++ src/layoutSidebar.js
@@ -1,30 +1,57 @@
 import React from 'react';
-import {Grid, Menu, Label, Input } from 'semantic-ui-react';
+import {Grid, Menu, Label, Input, Container } from 'semantic-ui-react';
 
 const style = {};
+const topMenuHeight = 50;
 const sidebarWidth = 250;
 const sidebarMarginLeft = 15;
-style.menu = {
+style.topMenu = {
+  position: 'fixed',
+  top: 0,
+  bottom: 0,
+  // left: sidebarMarginLeft + sidebarWidth,
+  left: 0,
+  height: topMenuHeight,
+  display: 'flex',
+};
+
+style.sidebarMenu = {
   position: 'fixed',
   display: 'flex',
   flexDirection: 'column',
-  top: 0,
+  top: topMenuHeight,
   bottom: 0,
   left: 0,
   marginLeft: sidebarMarginLeft,
   width: sidebarWidth + sidebarMarginLeft,
   overflowX: 'hidden',
+};
+
+style.sidebarMain = {
+  marginLeft: style.sidebarMenu.width,
+  marginTop: style.topMenu.height,
+  minWidth: style.sidebarMenu.width + 300,
 }
+
+
 
 class LayoutSidebar extends React.Component {
   render() {
     return (
       <div>
-        <div style={style.menu}>
+        <div style={style.topMenu}>
+          <div style={{ width: sidebarMarginLeft + sidebarWidth, }}></div>
+          <Menu text>
+            <Menu.Item>My Home</Menu.Item>
+            <Menu.Item>My Home</Menu.Item>
+            <Menu.Item>My Home</Menu.Item>
+          </Menu>
+        </div>
+
+        <div style={style.sidebarMenu}>
           <Menu
             vertical
             text
-
             >
             <Menu.Item name='inbox'>
               <Label color='teal'>1</Label>
@@ -46,7 +73,8 @@ class LayoutSidebar extends React.Component {
             </Menu.Item>
           </Menu>
         </div>
-        <div style={{ marginLeft: style.menu.width, }}>
+
+        <div style={style.sidebarMain}>
           <Grid column={1}>
             <Grid.Column>
 
