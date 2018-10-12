54ad777d62ef4e1d6f3276258a07c76a704472c2
Sha Liu <sha@snug.com>
2018-10-12 11:53:20 +1100

    add download application modal example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Modal } from 'semantic-ui-react'
+
+
+const ModalModalExample = () => (
+  <Modal size="tiny" trigger={<Button>Download Application</Button>}>
+    <Modal.Header>Download Application</Modal.Header>
+    <Modal.Actions>
+      <Button positive>Summary</Button>
+      <Button positive icon='cloud download' labelPosition='right' content='Attachments' />
+    </Modal.Actions>
+  </Modal>
+)
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ModalModalExample />
     );
   }
 }
